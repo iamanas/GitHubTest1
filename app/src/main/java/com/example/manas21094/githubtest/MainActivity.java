40a570cb1dc5a53d6db567fba50fd247268c5bc0
MANAS VIJAYWARGIYA <MANAS VIJAYWARGIYA>
2017-02-01 12:40:01 +0530

    Initial commit

---
+++ app/src/main/java/com/example/manas21094/githubtest/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.manas21094.githubtest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
